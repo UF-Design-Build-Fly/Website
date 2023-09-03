e677ed5e08073a4055f06cd5e73170650d7be220
KodingKat <kodingkatmc@gmail.com>
2023-09-03 17:27:12 -0400

    Nathaniel - GH Pages Deploy Attempt

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from './components/footer.js';
 
 //IMPORT PAGE: import Page from '../pages/PAGE.js';
 import Home from './pages/home.js';
+import Previous_Planes from './pages/previous_planes.js';
 
 import "./css/fonts.css"
 import './css/styles.css';
@@ -23,6 +24,7 @@ function App() {
 
         <Routes>
           <Route path={'/*'} element={<Home/>}/>
+          <Route path={'/previous_planes'} element={<Previous_Planes/>}/>
         </Routes>
 
       </HashRouter>
---
+++ src/pages/previous_planes.js
@@ -0,0 +1,15 @@
+function Home() {
+  document.title = "UF DBF - Previous Planes";
+
+  return (
+    <div id="holder">
+      <div className="content"></div>
+      <div className="content">
+        <div className="topPadding"/>
+        <h1 className="title">Previous Planes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
