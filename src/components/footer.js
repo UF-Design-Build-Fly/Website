91c283a78123e898ca1328edb72bf2d8e2bb937d
KodingKat <kodingkatmc@gmail.com>
2023-10-15 13:44:35 -0400

    Fixed spacing

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import "../css/footer.css";
 import instagramIcon from "../media/images/instagramIcon.png";
 import slackIcon from "../media/images/slackIcon.ico";
+import linkedIn from "../media/images/linkedIn.png";
 
 function Footer() {
   return (
@@ -18,10 +19,18 @@ function Footer() {
         <a
           className="socialLink"
           target="_about"
-          href="https://join.slack.com/t/ufdesignbuildfly/signup\"
+          href="https://join.slack.com/t/ufdesignbuildfly/signup/"
         >
           <img alt="slack_icon" className="socialImage" src={slackIcon} />
           <span className="socialText">Slack</span>
+        </a>
+        <a
+          className="socialLink"
+          target="_about"
+          href="https://www.linkedin.com/company/ufdbf/"
+        >
+          <img alt="slack_icon" className="socialImage" src={linkedIn} />
+          <span className="socialText">LinkedIn</span>
         </a>
       </div>
     </div>
